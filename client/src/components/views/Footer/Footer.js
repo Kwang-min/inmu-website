0ae0d200e9431c658124818645a885a8955d8e34
Kwang-min <j2h2677@gmail.com>
2021-05-20 12:28:28 +0900

    footer 문구 삽입

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -4,7 +4,12 @@ import './Footer.css'
 function Footer() {
     return (
         <div style={{backgroundColor: '#1b1c1d', height: '30vh'}}>
-            Footer
+            <div style={{ color: 'white', width: '70%', margin: '0 auto' }}>
+                <br/>
+                <p>인생뮤직</p>
+                <p>경기 화성시 동탄중심상가2길 5 | 0507-1478-5455</p>
+                <p>대표: 권대희 마승일</p>
+            </div>
         </div>
     )
 }
